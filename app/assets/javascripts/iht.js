cc8871aced33cbee1b54276bf2c0d0d01928bdee
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2018-07-23 16:24:23 +0100

    split functionality for js on or off.

--- app/assets/javascripts/iht.js
+++ app/assets/javascripts/iht.js
@@ -101,18 +101,20 @@ if($('[data-enhanced-select]').length > 0){
 // Handle the IHT progressive disclosure open/close functionality
 // =====================================================
  $("#getHelpDisclosure").one("click", function() {
+ console.log("click")
         $.ajax({url: reportProblemAjaxUrl, success: function(result){
+         $("#getHelpForm").html(result);
           }});
     });
-
-
- $(".report-error__toggle").on("click", function(e) {
-    if (document.getElementById("getHelpDisclosure").open) {
-            document.getElementById("getHelpDisclosure").open = false;
-     }else{
-            document.getElementById("getHelpDisclosure").open = true;
-         }
-     });
+//
+//
+// $(".report-error__toggle").on("click", function(e) {
+//    if (document.getElementById("getHelpDisclosure").open) {
+//            document.getElementById("getHelpDisclosure").open = false;
+//     }else{
+//            document.getElementById("getHelpDisclosure").open = true;
+//         }
+//     });
 
 // =====================================================
 
